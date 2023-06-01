1b6f92f7f79c3e8980ec9e3ddd255b4d4e322b98
mamula2023 <mamulashvili.2017@gmail.com>
2023-06-02 02:44:52 +0400

    easy

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,10 @@
 package lang.print.gaps.task5;
 
+import java.util.logging.Logger;
+
 public class TimeConvertor {
     public void convert(float minutes) {
+        float seconds = minutes * 60;
+        System.out.println(seconds);
     }
 }
